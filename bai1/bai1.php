e6c9d46198ad8d304ae34a8653c4c0cea60ea457
Trung-NBT <nguyenba29081997@gmail.com>
2019-05-23 14:08:26 +0700

    bai1

--- bai1/bai1.php
+++ bai1/bai1.php
@@ -10,9 +10,9 @@
     <h3>Giai Phương Trình Bậc Hai</h3>
 
     <form action="" method="POST" accept-charset="utf-8">
-        <input type="text" name="txta" style="width: 20px;" value=""> X<sub>2</sub> +
+        <input type="text" name="txta" style="width: 20px;" value=""> X<sup>2</sup> +
         <input type="text" name="txtb" style="width: 20px;" value=""> X +
-        <input type="text" name="txtc" value="">
+        <input type="text" name="txtc" style="width: 20px;" value=""> = 0
         <br><br>
         <input type="submit" name="result" value="KetQua">
     </form>
@@ -22,35 +22,35 @@
 
             $kq = '';
 
-            $a = $_POST['txta'];
-            $b = $_POST['txtb'];
-            $c = $_POST['txtc'];
+            $a = ($_POST['txta']=='') ? 0 :  $_POST['txta'];
+            $b = ($_POST['txtb']=='') ? 0 :  $_POST['txtb'];
+            $c = ($_POST['txtc']=='') ? 0 :  $_POST['txtc'];
 
-            if($a =='' || $b == '' || $c == '')
+            if(!is_numeric($a) || ! is_numeric($b) || ! is_numeric($c))
             {
-                echo "Nhập Đầy Đủ Data";
+                echo "Sai Dữ Liệu";
                 exit();
             }
-            else{
-                if(!is_numeric($a) || ! is_numeric($b) || ! is_numeric($c))
-                {
-                    echo "Sai Dữ Liệu";
-                    exit();
-                }
-                $delta = ($b*$b) - (4*$a*$c);
+            
+            echo $a = gptb2($a,$b,$c);
+            
+        }
 
-                if ($delta < 0){
-                    echo $kq = 'Phương trình vô nghiệp';
-                }
-                else if ($delta == 0){
-                    echo $kq = 'Phương trình nghiệp kép x1 = x2 = ' . (-$b/2*$a);
-                }
-                else {
-                    $kq = 'Phương trình có hai nghiệp phân biệt:'.'<br>'.'x1='.((-$b + sqrt($delta))/2*$a)."<br>".'x2 = '.((-$b - sqrt($delta))/2*$a);
-                    echo $kq;
-                }   
+        function gptb2($a,$b,$c)
+        {
+            $delta = ($b*$b) - (4*$a*$c);
+            if ($delta < 0){
+                $kq = 'Phương trình vô nghiệp';
             }
+            else if ($delta == 0){
+                $kq = 'Phương trình nghiệp kép x1 = x2 = ' . (-$b/2*$a);
+            }
+            else {
+                $kq = 'Phương trình có hai nghiệp phân biệt:'.'<br>'.'x1='.((-$b + sqrt($delta))/2*$a)."<br>".'x2 = '.((-$b - sqrt($delta))/2*$a);
+            }   
+            return $kq;
         }
     ?>
+
 </body>
 </html>
