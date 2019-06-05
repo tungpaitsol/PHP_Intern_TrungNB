fb32043bbfd9d5c677edfaeb8566788914e271fa
Trung-NBT <nguyenba29081997@gmail.com>
2019-06-05 13:54:08 +0700

    commit

--- bai5/bai5.php
+++ bai5/bai5.php
@@ -33,7 +33,6 @@
 		
 		if(isset($_POST["btntao"]))
 		{	
-
 			$number = isset($_POST['txtnum']) ? $_POST['txtnum'] : '';
 			if($number == '')
 			{
@@ -52,7 +51,6 @@
 				$_SESSION['number'] = $number;
 			}		
 		}
-
 		function create_array($number)
 		{
 			$name = array("SP_01","SP_02","SP_03","SP_04","SP_05","SP_06","SP_07","SP_08","SP_09");
@@ -61,7 +59,7 @@
 			$total = 0;
 			for ($i=0; $i < $number ; $i++) { 
 					$rand_name = $name[rand(0,8)];
-					$add_array = array($i,$rand_name,rand(0,5),rand(3,10),rand(100,105));	
+					$add_array = array($i,$rand_name,rand(100,1000),rand(10,20),rand(10,500));	
 					$total = $add_array[2]*$add_array[4];	
 					$add_array[] = $total;
 					$array_combine  = array_combine($arr_key, $add_array);
@@ -102,34 +100,44 @@
 			$array1 = sort_ASC($array1,$string1);
 			return $array1;
 		}
-		function edit_order($number,$array1,$array2,$string1) : array
-		{
-			for ($i = 0; $i < $number; $i ++) { 
-				for ($j = $i+1; $j < $number; $j ++) { 
-					if($array1[$i][$string1] != $array2[$i])
-					{
-						$array1[$i][$string1] = $array2[$i];
-
-					}	
-				}
-			}
-			return $array1;
-		}
-
+		// function edit_order($number,$array1,$array2,$string1) : array
+		// {
+		// 	for ($i = 0; $i < $number; $i ++) { 
+		// 		if($array1[$i][$string1] != $array2[$i])
+		// 		{
+		// 			$array1[$i][$string1] = $array2[$i];
+		// 		}	
+		// 	}
+		// 	return $array1;
+		// }
 		if(isset($_POST['btnsave']))
 		{
-			$_SESSION['mang'] = edit_order($_SESSION['number'],$_SESSION['mang'],$_POST['txtorder'],'order');
-
+			//$_SESSION['mang'] = edit_order($_SESSION['number'],$_SESSION['mang'],$_POST['txtorder'],'order');	
+			echo "<pre>";
+			$arr = array('id','order');
+			$newarr = array_chunk($_POST['txtorder'],1);
+			$arr_one = array();
+			$arr_two = array();
+			$arr_three = array();
+			foreach ($_SESSION['mang'] as $key => $value) {
+				array_push($arr_three, $value['id']);
+			}
+			for ($i=0; $i < $_SESSION['number']; $i++) {
+				array_unshift($newarr[$i], $arr_three[$i]);
+				$arr_one = array_combine($arr, $newarr[$i]);
+				array_push($arr_two, $arr_one);
+			}
+			for ($j=0; $j < $_SESSION['number']; $j++) {
+				if($_SESSION['mang'][$j]['id'] == $arr_two[$j]['id'])
+				{
+					$_SESSION['mang'][$j]['order'] = $arr_two[$j]['order'];
+				}	
+			}
 		}
-
 		if(isset($_POST['odtang'])){
 			$_SESSION['mang'] = order_ASC($_SESSION['number'],$_SESSION['mang'],'order','id');
-
 		}
-
-
-	?>
-	
+	?>	
 	<form action="" method="POST" accept-charset="utf-8">
 		<table style="margin: auto">
 			<caption>Danh Sách Sản Phẩm</caption>
@@ -150,12 +158,14 @@
 						<td><?php echo $value['name']?></td>
 						<td><?php echo $value['price']?></td>
 						<td><?php echo $value['quantity']?></td>
-						<td><input id="<?php echo $value['id']?>" type="text" name="txtorder[]" value="<?php echo $value['order']?>"></td>
-						<td><?php echo $value['Total']?></td>
+						<td><input id="<?php echo $value['id']?>" type="text" name="txtorder[]" value="<?php echo $value['order']?>">
+						</td>
+						
+						<td><?php echo $value['Total']?></td>	
 					</tr>
 				<?php endforeach; ?>
 					<tr>
-						<td><input type="submit" name="btnsave" value="Save"></td>
+						<td colspan="6"><input type="submit" style="width: 100px" name="btnsave" value="Save"></td>
 					</tr>
 			</tbody>
 		</table>
